63aa77ece20a35e97b4a837ccf643e8b29cdcd07
Ashish Singh <ashuisashish333@gmail.com>
2025-04-02 20:56:54 +0530

    couponController updated

---
+++ routes/couponRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { createCoupon, getAllCoupons, getCouponById, updateCoupon, deleteCoupon } from '../controllers/couponController.js';
+
+const router = express.Router();
+
+router.post('/', createCoupon);
+router.get('/', getAllCoupons);
+router.get('/:id', getCouponById);
+router.put('/:id', updateCoupon);
+router.delete('/:id', deleteCoupon);
+
+export default router;
